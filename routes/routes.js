0d791fe9bb7d3f96b87b4081914fb26c5efa4ab4
Marcosdicampli <dicamplimarcos@gmail.com>
2024-06-30 06:12:10 -0300

    starting

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+import { Router } from "express";
+
+const routes= Router()
+
+//endpoints
+
+routes.post("/")
+routes.get("/")
+routes.delete("/:id")
+routes.get("/:id")
+
+
+
+
+
+export default routes

4622f763e641a091d84141e7829b81d1afb2d071
Marcosdicampli <dicamplimarcos@gmail.com>
2024-07-01 18:57:16 -0300

    Starting

--- routes/routes.js
+++ routes/routes.js
@@ -1,14 +1,19 @@
 import { Router } from "express";
+import PalabrasController from "../controllers/PalabrasController.js";
 
 const routes= Router()
 
+const palabrasController=new PalabrasController()
+
 //endpoints
 
-routes.post("/")
-routes.get("/")
-routes.delete("/:id")
-routes.get("/:id")
+routes.post("/",palabrasController.createPalabra)
+routes.get("/",palabrasController.getPalabras)
+routes.delete("/:palabra",palabrasController.deletePalabra)
+//routes.get("/:id")
 
+//routes.delete("/:id")
+//routes.get("/:id")
 
 
 
